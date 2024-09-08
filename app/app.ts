44fe279d24fbb38c325b8ef41792d3539aa7b324
gleidistonaraujo18 <gleidistonaraujo18@gmail.com>
2024-09-08 01:54:45 -0300

    Iniciando com swagger - docker

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJson from './swagger.json'
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson))
 app.use('/api', router);
 
 export default app;
